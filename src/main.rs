a65f1ce1658b247a8eeb71ab779471d5c0b8a2bf
Jonathan Nilsson <jonathan@voysys.se>
2022-09-01 20:48:20 +0200

    Usb print

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,22 @@ use rp_pico::{
             bank0::{Gpio0, Gpio1},
             Output, Pin, PushPull,
         },
-        pac,
-        pwm::{FreeRunning, InputHighRunning},
+        pac::{self, interrupt},
+        pwm::InputHighRunning,
+        usb::UsbBus,
         Clock,
     },
 };
+
+use usb_device::{
+    class_prelude::UsbBusAllocator,
+    device::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
+};
+use usbd_serial::SerialPort;
+
+static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
+static mut USB_DEVICE: Option<UsbDevice<UsbBus>> = None;
+static mut USB_SERIAL: Option<SerialPort<UsbBus>> = None;
 
 fn set_up(out_1: &mut Pin<Gpio0, Output<PushPull>>, out_2: &mut Pin<Gpio1, Output<PushPull>>) {
     out_1.set_high().unwrap();
@@ -61,6 +72,33 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
+    unsafe {
+        USB_BUS = Some(UsbBusAllocator::new(UsbBus::new(
+            pac.USBCTRL_REGS,
+            pac.USBCTRL_DPRAM,
+            clocks.usb_clock,
+            true,
+            &mut pac.RESETS,
+        )));
+
+        USB_SERIAL = Some(SerialPort::new(USB_BUS.as_ref().unwrap()));
+
+        USB_DEVICE = Some(
+            UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0x16c0, 0x047a))
+                .manufacturer("JoNil")
+                .product("Pico Fork")
+                .serial_number("1")
+                .max_packet_size_0(64)
+                .device_class(2) // from: https://www.usb.org/defined-class-codes
+                .build(),
+        );
+
+        USB_DEVICE.as_mut().unwrap().force_reset().ok();
+
+        // Enable the USB interrupt
+        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
+    }
+
     let mut led_pin = pins.led.into_push_pull_output();
 
     let mut out_1 = pins.gpio0.into_push_pull_output();
@@ -74,6 +112,8 @@ fn main() -> ! {
     let count = pwm.get_counter();
 
     loop {
+        usb_print("Hej\n");
+
         set_up(&mut out_1, &mut out_2);
         led_pin.set_high().unwrap();
         delay.delay_ms(500);
@@ -86,4 +126,20 @@ fn main() -> ! {
         led_pin.set_high().unwrap();
         delay.delay_ms(2000);
     }
+}
+
+fn usb_print(str: &str) {
+    cortex_m::interrupt::free(|_| unsafe {
+        let serial = USB_SERIAL.as_mut().unwrap();
+        serial.write(str.as_bytes()).ok();
+    });
+}
+
+#[allow(non_snake_case)]
+#[interrupt]
+unsafe fn USBCTRL_IRQ() {
+    let usb_dev = USB_DEVICE.as_mut().unwrap();
+    let serial = USB_SERIAL.as_mut().unwrap();
+
+    usb_dev.poll(&mut [serial]);
 }
